dcb12cd8ce93428417a86e105124419c331a6cc8
Khoa-Pham-Van <khoapvhe181066@fpt.edu.vn>
2025-12-03 14:30:00 +0700

    d

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,10 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/',
   server: {
-    port: 3000,
-    open: true,
-    host: true
+    port: 3000
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: false,
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
+    emptyOutDir: true
   }
 })
